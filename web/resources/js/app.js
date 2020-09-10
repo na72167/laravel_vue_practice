e7954eea8fa34acca78cdad67b3a6b427e2423db
yh72167 <yh.19972372@gmail.com>
2020-09-10 23:30:50 +0900

    [add]ログイン画面の追加など

---
+++ web/resources/js/app.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+// ルーティングの定義をインポートする
+import router from './router'
+import store from './store' // ★　追加
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router, // ルーティングの定義を読み込む
+  store,
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
+})
+
---
+++ web/resources/js/pages/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container--small">
+
+    <ul class="tab">
+
+
+      <div class="panel" v-show="tab === 1">
+      <form class="form">
+          <label for="login-email">Email</label>
+          <input type="text" class="form__item" id="login-email" v-model="loginForm.email">
+          <label for="login-password">Password</label>
+          <input type="password" class="form__item" id="login-password" v-model="loginForm.password">
+          <div class="form__button">
+            <button type="submit" class="button button--inverse">login</button>
+          </div>
+        </form>
+      </div>
+
+
+
+      <li
+        class="tab__item"
+        :class="{'tab__item--active': tab === 2 }"
+        @click="tab = 2"
+      >Register</li>
+    </ul>
+
+
+    <div class="panel" v-show="tab === 1">Login Form</div>
+    <div class="panel" v-show="tab === 2">Register Form</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 1,
+      loginForm: {
+        email: '',
+        password: ''
+      },
+    }
+  },
+  methods: {
+    login () {
+      console.log(this.loginForm)
+    }
+  }
+}
+</script>
